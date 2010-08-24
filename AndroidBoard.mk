75ce3bbf55185d05bbc11af167f906aba6a56e40
cvpcs <root@cvpcs.org>
2010-08-24 10:01:22 -0500

    moved kernel sourcing to prevent variables from bleeding into other builds
    TARGET_PREBUILT_KERNEL was set in supersonic.mk, which caused it to be
    set across several different devices.  Thus when other device makefiles
    were sourced, they didn't set their default kernels as supersonic was
    including its own.
    
    For this reason, I moved the setting of TARGET_PREBUILT_KERNEL to AndroidBoard.mk,
    which is only sourced for each individual device.

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,28 +1,16 @@
-# Copyright (C) 2009 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-#
-# AndroidBoard.mk is a legacy mechanism to deal with a few
-# edge-cases that can't be managed otherwise. No new rules
-# should be added to this file.
-#
-
 LOCAL_PATH := $(call my-dir)
 
-# Least specific includes go first, so that they can get
-# overridden further down
-include $(CLEAR_VARS)
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel/zImage
 
-# include the non-open-source counterpart to this file
--include vendor/htc/supersonic/AndroidBoardVendor.mk
+# modules to include for default kernel
+PRODUCT_COPY_FILES += $(shell \
+        find $(LOCAL_PATH)/kernel -name '*.ko' \
+        | sed -r 's/^\/?(.*\/)([^/ ]+)$$/\1\2:system\/lib\/modules\/\2/' \
+        | tr '\n' ' ')
+endif
+
+file := $(INSTALLED_KERNEL_TARGET)
+ALL_PREBUILT += $(file)
+$(file): $(TARGET_PREBUILT_KERNEL) | $(ACP)
+	$(transform-prebuilt-to-target)
